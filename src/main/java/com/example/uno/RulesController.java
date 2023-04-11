8e0a17d0fc4c3a8fe0fe4b3b8877ffa51109248c
Tasnim Ferdous <tferdous611@gmail.com>
2023-04-10 22:10:56 -0400

    new rules view + controller

---
+++ src/main/java/com/example/uno/RulesController.java
@@ -0,0 +1,23 @@
+package com.example.uno;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class RulesController {
+
+    public Button btnReturnToTitle;
+
+    public void btnReturnToTitle_OnClick(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("start-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        window.setScene(scene);
+        window.show();
+    }
+}
