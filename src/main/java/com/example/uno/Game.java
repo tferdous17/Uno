23536bf491b2bc7ab3085c42aa624c13affec78d
Tasnim Ferdous <tferdous611@gmail.com>
2023-04-25 22:43:16 -0400

    file renames

--- src/main/java/com/example/uno/Game.java
+++ src/main/java/com/example/uno/Game.java
@@ -1,17 +1,24 @@
 package com.example.uno;
 
+import com.example.uno.card.Card;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
 
-public class Main extends Application {
+public class Game extends Application {
+    private static GameSettings gameSettings = new GameSettings();
+
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("start-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 975, 605);
+        FXMLLoader fxmlLoader = new FXMLLoader(Game.class.getResource("start-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Uno");
         stage.setScene(scene);
         stage.show();
@@ -20,4 +27,5 @@ public class Main extends Application {
     public static void main(String[] args) {
         launch();
     }
+
 }
