82d7952e3142c22fe7fe806ce5f3c49c12485349
Tasnim Ferdous <tferdous611@gmail.com>
2023-04-30 22:35:23 -0400

    added comment about imagePath

--- src/main/java/com/example/uno/card/Card.java
+++ src/main/java/com/example/uno/card/Card.java
@@ -3,13 +3,12 @@ package com.example.uno.card;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
-import java.util.Objects;
-
 public abstract class Card {
     protected CardColor color;
     protected int value;
     protected Image image;
     protected ImageView imageView;
+    // Change following file path if used on a different device
     protected String imagePath = "C:\\Users\\tferd\\Desktop\\Uno\\Uno\\src\\main\\resources\\com\\example\\uno\\imgs\\card_icons\\";
 
     public Card() {
@@ -59,6 +58,6 @@ public abstract class Card {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Card card = (Card) o;
-        return value == card.value || color == card.color;
+        return this.value == card.getValue() || this.color == card.getColor();
     }
 }
